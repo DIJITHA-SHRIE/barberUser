6d318028abf6eb4cb94fcb5154f2a7aec89d9dbd
Dijitha <dijitha@Dijithas-MacBook-Air.local>
2019-11-23 18:28:16 +0530

    Push Notification

--- app/src/main/java/com/barberapp/barberuser/utils/Constants.java
+++ app/src/main/java/com/barberapp/barberuser/utils/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 	public static final String PARAMETER_EQUALS = "=";
 	//public static final String TRANS_URL = "https://secure.ccavenue.com/transaction/initTrans";
 	public static final String TRANS_URL = "https://secure.ccavenue.com/transaction/initTrans";
+	public static final String FIREBASE_TOKEN = "FIREBASE_TOKEN";
 }
